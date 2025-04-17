b04a3d318e71650a038ba276519ad26d34e8e46a
dev-maeconomy <dev@maeconomy.org>
2025-04-17 22:03:49 +0300

    Changes : - define sslcontext outside from resttemplate config

--- uuid-common/src/main/java/io/recheck/uuidprotocol/common/resttemplate/config/RestTemplateImplBuilder.java
+++ uuid-common/src/main/java/io/recheck/uuidprotocol/common/resttemplate/config/RestTemplateImplBuilder.java
@@ -2,9 +2,7 @@ package io.recheck.uuidprotocol.common.resttemplate.config;
 
 import io.recheck.uuidprotocol.common.resttemplate.RestTemplateImpl;
 import io.recheck.uuidprotocol.common.resttemplate.config.logging.LoggingInterceptor;
-import io.recheck.uuidprotocol.common.resttemplate.model.SSLContextSpec;
 import io.recheck.uuidprotocol.common.resttemplate.model.ServerSpec;
-import lombok.SneakyThrows;
 import org.apache.hc.client5.http.config.TlsConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -13,35 +11,17 @@ import org.apache.hc.client5.http.io.HttpClientConnectionManager;
 import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
 import org.apache.hc.client5.http.ssl.TlsSocketStrategy;
 import org.apache.hc.core5.http.ssl.TLS;
-import org.apache.hc.core5.ssl.SSLContexts;
 import org.apache.hc.core5.util.Timeout;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.util.ResourceUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
 import java.util.Collections;
 
-public class RestTemplateImplConfig {
+public class RestTemplateImplBuilder {
 
-    @SneakyThrows
-    public RestTemplateImpl buildSSLRestTemplate(SSLContextSpec sslContextSpec, ServerSpec serverSpec) {
-        SSLContext sslContext;
-
-        if (StringUtils.hasText(sslContextSpec.getKeyStoreAlias())) {
-            sslContext = SSLContexts.custom()
-                    .loadTrustMaterial(ResourceUtils.getFile(sslContextSpec.getTrustStoreFile()), sslContextSpec.getTruststorePassword().toCharArray())
-                    .loadKeyMaterial(ResourceUtils.getFile(sslContextSpec.getKeyStoreFile()), sslContextSpec.getKeystorePassword().toCharArray(), sslContextSpec.getKeystorePassword().toCharArray(), (map, sslParameters) -> sslContextSpec.getKeyStoreAlias())
-                    .build();
-        } else {
-            sslContext = SSLContexts.custom()
-                    .loadTrustMaterial(ResourceUtils.getFile(sslContextSpec.getTrustStoreFile()), sslContextSpec.getTruststorePassword().toCharArray())
-                    .loadKeyMaterial(ResourceUtils.getFile(sslContextSpec.getKeyStoreFile()), sslContextSpec.getKeystorePassword().toCharArray(), sslContextSpec.getKeystorePassword().toCharArray())
-                    .build();
-        }
-
+    public RestTemplateImpl build(SSLContext sslContext, ServerSpec serverSpec) {
         final TlsSocketStrategy tlsStrategy = new DefaultClientTlsStrategy(sslContext);
         final HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
                 .setTlsSocketStrategy(tlsStrategy)
--- uuid-node-network/src/main/java/io/recheck/uuidprotocol/nodenetwork/owner/UUIDOwnerServiceConfig.java
+++ uuid-node-network/src/main/java/io/recheck/uuidprotocol/nodenetwork/owner/UUIDOwnerServiceConfig.java
@@ -1,21 +1,34 @@
 package io.recheck.uuidprotocol.nodenetwork.owner;
 
 import io.recheck.uuidprotocol.common.resttemplate.RestTemplateImpl;
-import io.recheck.uuidprotocol.common.resttemplate.config.RestTemplateImplConfig;
+import io.recheck.uuidprotocol.common.resttemplate.config.RestTemplateImplBuilder;
 import io.recheck.uuidprotocol.common.resttemplate.model.SSLContextSpec;
 import io.recheck.uuidprotocol.common.resttemplate.model.ServerSpec;
+import lombok.SneakyThrows;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+
+import javax.net.ssl.SSLContext;
+import java.security.cert.X509Certificate;
 
 @Configuration
 public class UUIDOwnerServiceConfig {
 
-    private RestTemplateImplConfig restTemplateImplConfig = new RestTemplateImplConfig();
+    private RestTemplateImplBuilder restTemplateImplBuilder = new RestTemplateImplBuilder();
 
     @Bean
+    @SneakyThrows
     public RestTemplateImpl restTemplateImpl(SSLContextSpec sslContextSpec, ServerSpec serverSpec) {
-        return restTemplateImplConfig.buildSSLRestTemplate(sslContextSpec, serverSpec);
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+        SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .loadKeyMaterial(ResourceUtils.getFile(sslContextSpec.getKeyStoreFile()), sslContextSpec.getKeystorePassword().toCharArray(), sslContextSpec.getKeystorePassword().toCharArray(), (map, sslParameters) -> sslContextSpec.getKeyStoreAlias())
+                .build();
+        return restTemplateImplBuilder.build(sslContext, serverSpec);
     }
 
     @Bean
