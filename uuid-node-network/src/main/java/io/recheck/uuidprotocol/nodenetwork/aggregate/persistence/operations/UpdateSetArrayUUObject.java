7afd6a65e7b7e9fa92803f60fea7a66d0cca195b
Ivaylo Rusev <irusev@recheck.io>
2025-05-29 15:59:03 +0300

    Changes: - Fix MongoUtils#convertToDocument (for collections instead of null write empty array) - Fix historyNodes for aggregation to be converted into mongodb documents

--- uuid-node-network/src/main/java/io/recheck/uuidprotocol/nodenetwork/aggregate/persistence/operations/UpdateSetArrayUUObject.java
+++ uuid-node-network/src/main/java/io/recheck/uuidprotocol/nodenetwork/aggregate/persistence/operations/UpdateSetArrayUUObject.java
@@ -1,20 +1,19 @@
 package io.recheck.uuidprotocol.nodenetwork.aggregate.persistence.operations;
 
+import io.recheck.uuidprotocol.common.mongodb.MongoUtils;
 import io.recheck.uuidprotocol.domain.node.model.UUObject;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
-import java.util.List;
-
-public class UpdateSetArrayUUObject extends AbstractOperation<List<UUObject>, UUObject, UUObject>{
+public class UpdateSetArrayUUObject extends AbstractOperation<UUObject, UUObject, UUObject>{
     public UpdateSetArrayUUObject(String path) {
         super(path);
     }
 
     @Override
-    protected void updateOperation(Update update, List<UUObject> updateObject) {
-        update.set(path, updateObject);
+    protected void updateOperation(Update update, UUObject updateObject) {
+        update.push(path, MongoUtils.convertToDocument(updateObject));
     }
 
     @Override
